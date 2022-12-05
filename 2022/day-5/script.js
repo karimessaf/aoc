abe6443de74ece5e353fde281e0660e9bb990470
karim essaf <remagkes@gmail.com>
2022-12-05 17:51:04 +0100

    correct day-5

--- 2022/day-5/script.js
+++ 2022/day-5/script.js
@@ -18,7 +18,7 @@ moves2DArray.pop()
 // Part 1
 const part1Crates = cloneDeep(crates2DArray)
 moves2DArray.map(move => {
-  Array.from(Array(parseInt(move[0]))).map((_, i) => {
+  Array.from(Array(parseInt(move[0]))).forEach(_ => {
     const crate = part1Crates[move[1] - 1].shift()
     part1Crates[move[2] - 1].unshift(crate)
   })
@@ -29,22 +29,31 @@ console.log('part1Result', part1Result)
 // Part 2
 const part2Crates = cloneDeep(crates2DArray)
 moves2DArray.map(move => {
-  console.log(`NEW MOVE (moving ${move[0]}):`)
+  const crates = part2Crates[move[1] - 1].splice(0, move[0])
+  part2Crates[move[2] - 1].unshift(...crates)
+})
+
+const part2Result = part2Crates.map(crate => crate[0]).join('')
+console.log('\npart2Result', part2Result)
+
+// Part 3?
+/* for whatever reason, when reading part 2, I thought that the crane can move a maximum of 3 crates,
+  so the algorithm was quite complex */
+const part3Crates = cloneDeep(crates2DArray)
+moves2DArray.map(move => {
+  console.log(`\n---NEW MOVE (move ${move[0]} from ${move[1]} -> ${move[2]})`)
   let remainingCrates = move[0]
   for (let i = 0; i < Math.ceil(move[0] / 3); i++) {
     let movedCrates = []
     console.log(`iteration ${i+1}/${Math.ceil(move[0] / 3)}`)
+    console.log(`from: ${part3Crates[move[1] - 1]}`)
     i === 0
-      ? movedCrates = part2Crates[move[1] - 1].splice(0, Math.min(3, move[0])) // first move (1, 2, or 3)
-      : movedCrates = part2Crates[move[1] - 1].splice(0, remainingCrates > 3 ? 3 : remainingCrates) // following moves (if +3 crates)
+      ? movedCrates = part3Crates[move[1] - 1].splice(0, Math.min(3, move[0])) // first move (1, 2, or 3)
+      : movedCrates = part3Crates[move[1] - 1].splice(0, remainingCrates > 3 ? 3 : remainingCrates) // following moves (if +3 crates)
     remainingCrates -= 3
-    part2Crates[move[2] - 1].unshift(...movedCrates)
-    console.log(`from: ${part2Crates[move[1] - 1]}`)
+    part3Crates[move[2] - 1].unshift(...movedCrates)
     console.log(`crates: ${movedCrates} (remaining: ${remainingCrates})`)
-    console.log(`to: ${part2Crates[move[2] - 1]}`)
+    console.log(`to: ${part3Crates[move[2] - 1]}`)
     console.log('')
   }
 })
-
-const part2Result = part2Crates.map(crate => crate[0]).join('')
-console.log('part2Result', part2Result)
