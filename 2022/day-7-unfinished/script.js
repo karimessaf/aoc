3ae8412a5c3e2e5aef50bb4991357ec4beb25147
karimessaf <karim.essaf@gmail.com>
2022-12-09 01:59:17 +0100

    update day-7 script draft

--- 2022/day-7-unfinished/script.js
+++ 2022/day-7-unfinished/script.js
@@ -1,37 +1,43 @@
 // https://adventofcode.com/2022/day/7
 
-// Parse file
 const fs = require('fs')
+const groupBy = require('lodash/groupBy')
+const uniq = require('lodash/uniq')
+const sum = require('lodash/sum')
+
 const file = fs.readFileSync(require.resolve('./sample.txt'), 'utf-8')
 const array = file.split('\n')
 array.pop()
 
-const tree = []
+let folders = []
+/*
+  folders format: [
+    { name: '/', size: 0, parentIndex: -1, childrenIndexes: [0, 1] },
+    { name: 'a', size: 0, parentIndex: 0, childrenIndexes: [2] }
+    { name: 'b.txt', size: 123  parentIndex: 1 }
+  ]
+*/
 
-handleCommand = (line, index) => {
-  const _line = line.split(' ')
-  if (_line[0] !== '$' || _line[1] !== 'cd') return // ignore non cd commands
-  tree.push({ node: _line[2], children: getChildren(line, index)})
-  // if (_line[2] === '..')
-  // console.log(_line)
-}
-
-getChildren = (line, index) => {
-  const children = []
-  for (let i = 1; i <= array.length; i++) {
-    console.log(`(getChildren) i:${i}, array[index+i]: ${array[index+i]}, index+i:${index+i}, array:${array[index+i]}`)
-    if (index + i === array.length) return
-    if (array[index + i].split(' ')[1] === 'ls') continue; // skip ls
-    if (array[index + i].split(' ')[1] === 'cd') handleCommand(array[index + i], index + i); // parent: cd, child: cd
-    children.push(array[index + i])
+createFolder = (parentIndex) => {
+  for (let i = 1; i <= array.length - parentIndex + 1; i++) {
+    if (i + parentIndex === array.length - 1) break // overflow
+    const child = array[i + parentIndex + 1].split(' ')
+    if (child[0] === '$') break // skip command
+    folders.push({ name: child[1], size: child[0] === 'dir' ? 0 : child[0], parentIndex })
   }
-  return children
 }
 
+array.map((line, i) => (line.split(' ')[1] === 'ls') && createFolder(i - 1)) // create folders when command is 'ls'
+const parentIndexes = uniq(folders.map(f => f.parentIndex))
 
-handleCommand(array[0], 0)
-// array.forEach((line, index) => {
-//   handleCommand(line, index)
-// })
+const folderSums = parentIndexes.forEach(index => folders.filter(f => f.parentIndex === index).map(f => f.size))
 
-console.log('tree', tree)
+console.log('folderSums', folderSums)
+// const groupedFolders = groupBy(folders, 'parentIndex')
+
+// calculate size of folders with only files
+// groupedFolders.forEach(f => console.log(f))
+
+// console.log(groupedFolders)
+// console.log(folders)
+
